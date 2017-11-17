5d7e17cf0422a9633642f55c0ed106f4548f218f
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-11-17 10:06:08 -0600

    updated weather img route

--- src/Show/Show.js
+++ src/Show/Show.js
@@ -6,7 +6,7 @@ class Show extends Component {
 	render() {
 		const cityData = this.props.weather.map((weather, i) => {
 			return <div className="card" key={i}>
-							<img className="showImg card-img-top" alt={weather.weather[0].description} src={'http://openweathermap.org/img/w/'+ weather.weather[0].icon + '.png'}></img>
+							<img className="showImg card-img-top" alt={weather.weather[0].description} src={'https://openweathermap.org/img/w/'+ weather.weather[0].icon + '.png'}></img>
 						<div className="card-body">
 							<h4 className="showTitle"> Current weather in {weather.name}</h4>
 							<p className="showLi">{weather.weather[0].main}</p>
